40d34afe686829c3898be651017e27825b4874a9
FrankJuanI <juan.frank@gm2dev.com>
2023-08-02 09:52:01 -0300

    fix en script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-let tabList = document.querySelectorAll(".tabs-container");
-let tabDesList = document.querySelectorAll(".description-tabs-container");
-
-console.log(tabList.event);
-console.log(tabDesList.event);
+let tabList = document.querySelectorAll(".tab");
+let tabDesList = document.querySelectorAll(".description-tab");
 
 tabList.forEach((tab) => {
   tab.addEventListener("click", (event) => {
     tabList.forEach((tabElement) => {
       tabElement.classList.remove("active");
     });
-    tabDeslist.forEach((descriptionElement) => {
+    tabDesList.forEach((descriptionElement) => {
       descriptionElement.classList.remove("active");
-      if (descriptionElement.id === `content-${event.target.id}`) {
+      if (descriptionElement.id === `des-${event.target.id}`) {
         descriptionElement.classList.add("active");
       }
     });
